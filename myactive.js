574dd4509b0cae0d9338e3d7ed73980b3c3fa891
JakeGorilla <j255121@gmail.com>
2016-12-05 23:29:48 +0800

    JS: add tab_map

--- myactive.js
+++ myactive.js
@@ -1,6 +1,23 @@
+var tab_map = {};
+
 function init_tags(){
-    
+    var tabs_arr = document.querySelectorAll('.mdl-layout__tab-panel');
+    //console.log(tabs_arr);
+    for (var i=0; i < tabs_arr.length; i++){
+        //console.log(tabs_arr[i]);
+        var tab_id = tabs_arr[i].getAttribute('id');
+        //console.log(tab_id);
+        tab_map[tab_id] = [];
+        var img_list = tabs_arr[i].querySelectorAll('.img_container');
+        //console.log(img_list);
+        for (var j=0; j < img_list.length; j++){
+            tab_map[tab_id].push(img_list[j].getAttribute('id'));
+        }
+    }
+    console.log(tab_map);
 }
+
+init_tags();
 
 function activate(e){
     var p = document.querySelector('#scroll-tab-1 .img_displayer').children;
